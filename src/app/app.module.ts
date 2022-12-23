2a6a2bdacfe59ad9424b5440a4544b3f2f8728f4
Mohammed Khaled <58882363+MK167@users.noreply.github.com>
2022-12-23 02:13:36 +0200

    add style structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './Pages/home/home.component';
+import { NavbarComponent } from './Core/components/navbar/navbar.component';
+import { FooterComponent } from './Core/components/footer/footer.component';
+import { SliderComponent } from './Core/components/slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    SliderComponent,
   ],
   imports: [
     BrowserModule,
